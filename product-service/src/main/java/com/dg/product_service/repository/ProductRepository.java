bfaa518737b56dfb5c1f55d01e0696b2d9efa76a
Dev-G29 <103144586+Dev-G29@users.noreply.github.com>
2025-01-26 23:47:52 +0530

    Delete product-service directory

--- product-service/src/main/java/com/dg/product_service/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dg.product_service.repository;
-
-import com.dg.product_service.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
